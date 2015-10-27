d50279541a8f3bd842b1b63c7e72f9ce30921519
Willian Vieira <willianvieirarosa@gmail.com>
2015-10-26 22:55:24 -0200

    house-password solved

---
+++ house-password.py
@@ -0,0 +1,15 @@
+import re
+def checkio(data):    
+    return not re.compile(r"(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])")\
+        .match(data) is None\
+        and data.__len__() >= 10
+​
+​
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(u'A1213pokl') == False, "1st example"
+    assert checkio(u'bAse730onE4') == True, "2nd example"
+    assert checkio(u'asasasasasasasaas') == False, "3rd example"
+    assert checkio(u'QWERTYqwerty') == False, "4th example"
+    assert checkio(u'123456123456') == False, "5th example"
+    assert checkio(u'QwErTy911poqqqq') == True, "6th example"
